81874b48dfc473ecc7bde3cb25420d5c9151b15a
bimbellb3r <bimbellb3r@gmail.com>
2024-03-19 06:22:22 +0800

    ubah page

--- app/dashboard/(overview)/page.js
+++ app/dashboard/(overview)/page.js
@@ -4,24 +4,25 @@ import { Suspense } from "react";
 import { CardsSkeleton } from "@/app/ui/skeletons";
 import { CardWrapper } from "@/app/ui/dashboard/cards";
 
-export default async function Page() {
-  const newRow = {
-    tanggal: "2024-03-17",
-    aktivitas: "Libur di rumahkuuuu",
-    cerita: "Seru sekaliiiii",
-  };
-  const data = await getDataSheet();
+// export default async function Page() {
+export default function Page() {
+  // const newRow = {
+  //   tanggal: "2024-03-17",
+  //   aktivitas: "Libur di rumahkuuuu",
+  //   cerita: "Seru sekaliiiii",
+  // };
+  // const data = await getDataSheet();
   // await appendToSpreadsheet(newRow);
   // await delDataSheet(newRow.tanggal);
   // await updateDataSheet(newRow);
   return (
     <>
       <p className={`text-center ${inter.className}`}>HOME</p>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
+      {/* <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3">
         <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
-      </div>
+      </div> */}
     </>
   );
 }
